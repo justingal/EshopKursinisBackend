9a6bd1234f7c85e35851cc53558e4c353aea33b6
Justinas Galcius <justinas.galcius@stud.vilniustech.lt>
2024-08-06 13:45:27 +0300

    first commit

---
+++ src/main/java/com/coursework/eshopkursinisbackend/EshopKursinisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coursework.eshopkursinisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopKursinisBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopKursinisBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coursework/eshopkursinisbackend/EshopKursinisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework.eshopkursinisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopKursinisBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
