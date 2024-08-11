abef9689ddb59be52485940b458487a799e0a1dc
codelotusr <smaliukasdeividas@gmail.com>
2024-08-11 20:22:13 +0300

    Fixed CommentDeserializer recursion bug

--- src/main/java/com/coursework/eshopkursinisbackend/config/CommentDeserializer.java
+++ src/main/java/com/coursework/eshopkursinisbackend/config/CommentDeserializer.java
@@ -10,9 +10,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class CommentDeserializer extends JsonDeserializer<Comment> {
-
     @Override
     public Comment deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
         JsonNode node = jp.getCodec().readTree(jp);
@@ -24,11 +24,21 @@ public class CommentDeserializer extends JsonDeserializer<Comment> {
 
         ((ObjectNode) node).remove(type);
 
-        return switch (type) {
-            case "Review" -> jp.getCodec().treeToValue(node, Review.class);
-            case "Comment" -> jp.getCodec().treeToValue(node, Comment.class);
-            default -> throw new IOException("Invalid comment type: " + type);
-        };
+        if ("Comment".equals(type)) {
+            Comment comment = new Comment();
+            comment.setCommentBody(node.get("commentBody").asText());
+            comment.setCommentTitle(node.get("commentTitle").asText());
+            JsonNode dateCreatedNode = node.get("dateCreated");
+            if (dateCreatedNode != null) {
+                String dateCreatedStr = dateCreatedNode.asText();
+                LocalDate dateCreated = LocalDate.parse(dateCreatedStr);
+                comment.setDateCreated(dateCreated);
+            }
+            return comment;
+        } else if ("Review".equals(type)) {
+            return jp.getCodec().treeToValue(node, Review.class);
+        } else {
+            throw new IOException("Invalid comment type: " + type);
+        }
     }
-
 }
